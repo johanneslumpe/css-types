2b3cc38f4ffea0558ffab87768cf6090cbb2b955
Johannes Lumpe <johannes@johanneslumpe.de>
2019-05-27 14:15:11 -0400

    update jest config

--- jest.config.js
+++ jest.config.js
@@ -1,20 +1,32 @@
-const { defaults } = require('jest-config');
-
 module.exports = {
-  roots: ['<rootDir>/src'],
-  transform: {
-    '^.+\\.tsx?$': 'ts-jest',
-  },
-  moduleFileExtensions: [...defaults.moduleFileExtensions, 'ts', 'tsx'],
-  testMatch: ['**/__tests__/**/*.ts?(x)'],
+  roots: [
+    '<rootDir>/src',
+  ],
+  moduleFileExtensions: [
+    'js',
+    'json',
+    'jsx',
+    'node',
+    'ts',
+    'tsx',
+  ],
+  testMatch: [
+    '**/__tests__/**/*.ts?(x)',
+  ],
   testURL: 'http://localhost',
-  testPathIgnorePatterns: ['/node_modules', '__tests__/utils'],
+  testPathIgnorePatterns: [
+    '/node_modules',
+    '__tests__/utils',
+  ],
   coverageDirectory: './coverage/',
-  collectCoverageFrom: ['src/**/*.{ts,tsx}'],
+  collectCoverageFrom: [
+    'src/**/*.{ts,tsx}',
+  ],
   collectCoverage: true,
   globals: {
     'ts-jest': {
-      tsConfigFile: 'tsconfig.cjs.test.json',
+      tsConfig: 'tsconfig.cjs.test.json',
     },
   },
-};
+  preset: 'ts-jest',
+}
